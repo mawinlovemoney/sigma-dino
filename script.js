702717066bf293341db012081c0801f0bee6f5df
mawinlovemoney <mawin.kun7662@gmail.com>
2025-06-26 11:44:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,47 @@ let gameRunning = false;
 let positon = 0;
 let velocity = 0;
 let gravity = -0.6;
+let jumping = false;
 let obstacleSpeed = 5;
+let obstaclespeed = 5;
 let moveInterval = null;
 let gameloopInterval = null;
+
+function startGame(){
+     score = 0;
+     velocity = 0;
+     positon = 0;
+     obstacleleft = 600;
+     obstacleSpeed = 5;
+     gameRunning = ture;
+     jumping = faluse;
+     
+     dino.style.bottom = positon + "px";
+     
+     startbutton.style.display = "none";
+     resetbutton.style.display = "inline";
+
+     gameloop();
+
+}
+
+
+
+function jumping(){
+    if(!jumping && gameRunning){
+        velocity = 12;
+        jumping = ture;
+    }
+}
+
+document.addEventListener("keydown", e =>{
+     if (e.code ==="space" || e.code ==="arrowUp"){
+          jumping();
+     }
+});
+
+startbutton.addEventListener("click", stratGame);
+resetbutton.addEventListener("click", ()=>{
+     startBtn.style.display = "inline";
+     resetbutton.style.display = "none";
+});
